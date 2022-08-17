f4fbe0a5dc7f558fb8c241f7aaab4c9623f41f76
Tyric Jones <tyrictjones@gmail.com>
2022-08-17 12:15:06 -0400

    show highs score input after answering all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var quizBank = [
         id: 0,
         question: 'Which keyword will not allow you to create a new variable?',
         choices: [
-            {text: 'var', correct: 0},
+            {text: 'var', correct: 0}, //1 is correct, 0 is incorrect
             {text: 'let', correct: 0},
             {text: 'command', correct: 1},
             {text: 'const', correct: 0},
@@ -31,16 +31,27 @@ var quizBank = [
     }
 ];
 
-//console.log(quizBank[1].question);
-//console.log(quizBank[1].choices[0].text);
-
-var welcomeEl = document.querySelector('.welcome');
+//capture each of the divs in the main section so that we can hide/display them as needed
+var welcomeEl = document.querySelector('#welcome');
 var startButtonEl = document.querySelector('#start-quiz');
 var questionBoxEl = document.querySelector('#question-box');
+var endQuizEl = document.querySelector('#end-quiz');
+var highScoresEl = document.querySelector('#high-scores');
 var answerFeedbackEl = document.querySelector('#feedback');
-var quizProgress = 0;
-var quizTimer = 100;
 
+
+//the quizProgress lets us know which question we're on the quizBank object
+var quizProgress = 0;
+
+
+//count down from a number and end the quiz if we reach 0 before finishing all the questions
+//create the timer variable here and dynamically populate it in the HTML as it changes
+var quizTimer = 100;
+var quizTimerEl = document.querySelector('#timer');
+quizTimerEl.textContent = quizTimer;
+
+
+//capture each element of the question li to later populate with text from the quizBank
 var questionText = document.querySelector('#question-text');
 var choice1 = document.querySelector('#choice1');
 var choice2 = document.querySelector('#choice2');
@@ -50,7 +61,7 @@ var choice4 = document.querySelector('#choice4');
 
 
 var startQuiz = function () {
-    //hide welcome div, display question-box div
+    //hide welcome div, show question div during the quiz; all divs start as display:none, change as needed
     welcomeEl.style.display = 'none';
     questionBoxEl.style.display = 'block';
 
@@ -58,6 +69,7 @@ var startQuiz = function () {
     loadQuestion(quizProgress);
     quizProgress++;
 
+    //capture all clicks ont the question-box div, if a list-item is clicked check the answer
     questionBoxEl.addEventListener('click', checkAnswer);
 
 };
@@ -65,12 +77,14 @@ var startQuiz = function () {
 
 
 var loadQuestion = function(quizProgress) {
+    //using global value of quizProgress as the index, get question text and fill into corresponding elements
     questionText.textContent = quizBank[quizProgress].question;
     choice1.textContent = quizBank[quizProgress].choices[0].text;
     choice2.textContent = quizBank[quizProgress].choices[1].text;
     choice3.textContent = quizBank[quizProgress].choices[2].text;
     choice4.textContent = quizBank[quizProgress].choices[3].text;
 
+    //create attribute to store our 1s and 0s to check the answer
     choice1.setAttribute('data-correct', quizBank[quizProgress].choices[0].correct);
     choice2.setAttribute('data-correct', quizBank[quizProgress].choices[1].correct);
     choice3.setAttribute('data-correct', quizBank[quizProgress].choices[2].correct);
@@ -81,6 +95,8 @@ var loadQuestion = function(quizProgress) {
 
 
 var checkAnswer = function() {
+    //if a list-item / answer choice is clicked, check whether its data-correct attribute is correct(1) or incorrect(0)
+    //if incorrect subtract 10 from the timer
     var targetEl = event.target;
     console.log(targetEl);
     var verifyCorrect = targetEl.getAttribute('data-correct');
@@ -99,8 +115,14 @@ var checkAnswer = function() {
         startQuiz();
     }
     else {
-        console.log('Quiz complete');
+        endQuiz();
     }
+};
+
+var endQuiz = function() {
+    questionBoxEl.style.display = 'none';
+    endQuizEl.style.display = 'block';
+
 };
 
 
